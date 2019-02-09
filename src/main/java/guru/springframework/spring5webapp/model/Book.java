d20a664a03061e3037c3dd37666821075b00eb85
Dre <dremon7@hotmail.com>
2019-02-09 13:38:15 -0500

    h2 added publisher

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -9,14 +9,15 @@ import java.util.Set;
 public class Book {
 
 
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String title;
     private String isbn;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -27,20 +28,19 @@ public class Book {
 
     }
 
-    public Book(String title, String isbn, String publisher){
+    public Book(String title, String isbn, Publisher publisher){
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
     }
 
-    private String publisher;
 
 
     public Long getId() {
@@ -59,8 +59,12 @@ public class Book {
         return isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Set<Author> getAuthors() {
@@ -75,9 +79,7 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
+
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
